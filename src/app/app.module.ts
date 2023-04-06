76cb8a8ac5a737df9ca8275775f32e6fe77162f8
tony tolver <andrademaresantonio00@gmail.com>
2023-04-05 18:14:31 -0600

    Estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowComponent } from './pages/show/show.component';
+import { CreateComponent } from './pages/create/create.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { GenericFormComponent } from './components/generic-form/generic-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowComponent,
+    CreateComponent,
+    EditComponent,
+    GenericFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/notes.md
@@ -0,0 +1,8 @@
+# instalaciones
+alertifyjs
+
+# bootstrap
+npm i bootstrap --save
+
+# font-awesome  -> free solid icons
+ng add @fortawesome/angular-fontawesome
